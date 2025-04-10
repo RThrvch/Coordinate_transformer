b5f91fc7b56d66091c650a6ffa75af9fdca493ac
Ruslan Mukhametsafin <muhametsafin.r@yandex.ru>
2025-04-11 01:09:59 +0500

    Changes in Doxygen docs

--- docs/doxygen/html/search/all_15.js
+++ docs/doxygen/html/search/all_15.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['параметры_20конфигурации_20tt_20config_20yaml_20tt_0',['Параметры конфигурации (&lt;tt&gt;config.yaml&lt;/tt&gt;)',['../index.html#autotoc_md6',1,'']]]
+  ['параметры_20конфигурации_20tt_20config_20yaml_20tt_0',['Параметры конфигурации (&lt;tt&gt;config.yaml&lt;/tt&gt;)',['../index.html#autotoc_md6',1,'']]],
+  ['пример_20использования_1',['Пример использования',['../index.html#autotoc_md9',1,'']]]
 ];
